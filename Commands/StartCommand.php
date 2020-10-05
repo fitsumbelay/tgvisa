3ed43cbe3c3751116bab4983c62590c393a25c62
unknown <fitsum.ozo@gmail.com>
2020-10-05 06:08:36 -0700

    edit start

--- Commands/StartCommand.php
+++ Commands/StartCommand.php
@@ -53,16 +53,46 @@ class StartCommand extends SystemCommand
      */
     public function execute()
     {
-        $message = $this->getMessage();
+        // $message = $this->getMessage();
 
-        $chat_id = $message->getChat()->getId();
-        $text    = 'Hi there!' . PHP_EOL . 'Type /help to see all commands!';
+        // $chat_id = $message->getChat()->getId();
+        // $text    = 'Hi there!' . PHP_EOL . 'Type /help to see all commands!';
 
-        $data = [
-            'chat_id' => $chat_id,
-            'text'    => $text,
-        ];
+        // $data = [
+        //     'chat_id' => $chat_id,
+        //     'text'    => $text,
+        // ];
 
-        return Request::sendMessage($data);
+        // return Request::sendMessage($data);
+
+
+        $keyboards[] = new Keyboard([
+            ['text' => 'REGISTER NOW'],
+          
+        ]);
+
+        $keyboard = end($keyboards)
+        ->setResizeKeyboard(true)
+        ->setOneTimeKeyboard(true)
+        ->setSelective(false);
+
+         $this->replyToChat( 
+            ' Welcome to AMERICAN MIGRATION BUREAU service!' . PHP_EOL .
+            'We offer a FREE online referral service to point you' . PHP_EOL . 
+            'to an independent ICCRC Immigration Consultant to help guide you through ' . PHP_EOL . 
+            'the often complex and time-consuming visa application process.' . PHP_EOL . 
+            ' The Migration Bureau does not offer any immigration advice itself,' . PHP_EOL . 
+            'but you contract directly with a registered advisor.' . PHP_EOL .' ' . PHP_EOL . 
+
+            'For over 20 years, the MIGRATION BUREAU has referred and helped 30,000 new settlers' .PHP_EOL . 
+             'with their dream of living and working abroad. Today, we are one of American`s ' .PHP_EOL . 
+             ' leading migration agent referral networks, also offering relocation and settlement referral services.' .PHP_EOL . 
+
+             ' * Please REGISTER NOW Below', [
+                'reply_markup' => $keyboard,
+             ]
+            
+            
+        );
     }
 }
